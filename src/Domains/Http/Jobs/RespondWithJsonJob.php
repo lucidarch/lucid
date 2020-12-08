8c5ec43b6ca150b8b7386597380c035730a9fb1d
Abed Halawi <halawi.abed@gmail.com>
2020-12-08 23:59:11 +0200

    add built-in jobs for responding to HTTP requests

---
+++ src/Domains/Http/Jobs/RespondWithJsonJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lucid\Domains\Http\Jobs;
+
+use Lucid\Units\Job;
+use Illuminate\Routing\ResponseFactory;
+
+class RespondWithJsonJob extends Job
+{
+    protected $status;
+    protected $content;
+    protected $headers;
+    protected $options;
+
+    public function __construct($content, $status = 200, array $headers = [], $options = 0)
+    {
+        $this->content = $content;
+        $this->status = $status;
+        $this->headers = $headers;
+        $this->options = $options;
+    }
+
+    public function handle(ResponseFactory $factory)
+    {
+        $response = [
+            'data' => $this->content,
+            'status' => $this->status,
+        ];
+
+        return $factory->json($response, $this->status, $this->headers, $this->options);
+    }
+}
