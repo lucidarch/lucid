f3317032c845a85a54e350e41fb6a2648ed095fd
Abed Halawi <2647333+Mulkave@users.noreply.github.com>
2021-12-16 18:34:35 +0200

    use mixed data type for run($unit, ...) to calm IDE

--- src/Bus/UnitDispatcher.php
+++ src/Bus/UnitDispatcher.php
@@ -23,13 +23,13 @@ trait UnitDispatcher
      * When the $arguments is an instance of Request
      * it will call dispatchFrom instead.
      *
-     * @param string                         $unit
+     * @param mixed                         $unit
      * @param array|\Illuminate\Http\Request $arguments
      * @param array                          $extra
      *
      * @return mixed
      */
-    public function run($unit, $arguments = [], $extra = [])
+    public function run(mixed $unit, $arguments = [], $extra = [])
     {
         if ($arguments instanceof Request) {
             $result = $this->dispatch($this->marshal($unit, $arguments, $extra));
